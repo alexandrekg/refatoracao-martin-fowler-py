c16eec332720fe77b5f2ff7d5c2dc484c7f8ee43
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2025-05-21 11:22:56 -0300

    <procedimento> extrair função - extraindo lógica do volume_credits_for para uma função separada

--- capitulo1/main.py
+++ capitulo1/main.py
@@ -6,19 +6,14 @@ def statement():
     total_amount = 0
     volume_credits = 0
     invoices = json.load(open('invoices.json'))[0]
-    
+
     result = f"Statement for {invoices['customer']} \n"
     for perf in invoices['performances']:
         # soma créditos por volume
-        volume_credits += max(perf['audience'] - 30, 0)
-
-        # soma um crédito extra para cada dez espectadores de comédia
-        if play_for(perf)['type'] == "comedy":
-            volume_credits += math.floor(perf['audience'] / 5)
-
+        volume_credits += volume_credits_for(perf)
         # exibe a linha para esta requisição
         result += f" {play_for(perf)['name']}: ${'{:,.2f}'.format(amount_for(perf) / 100)} ({perf['audience']} seats)\n"
-        total_amount +=  amount_for(perf)
+        total_amount += amount_for(perf)
     result += f"Amount owed is ${'{:,.2f}'.format(total_amount / 100)}\n"
     result += f"You earned {volume_credits} credits\n"
     return result
@@ -27,6 +22,7 @@ def statement():
 def play_for(a_performance):
     plays = json.load(open('plays.json'))
     return plays[a_performance['playID']]
+
 
 def amount_for(a_performance):
     """
@@ -44,8 +40,17 @@ def amount_for(a_performance):
         result += 300 * a_performance['audience']
     else:
         raise Exception(f"unknown type: {play_for(a_performance)['type']}")
-    
+
     return result
+
+
+def volume_credits_for(perf):
+    volume_credits = 0
+    volume_credits += max(perf['audience'] - 30, 0)
+    # soma um crédito extra para cada dez espectadores de comédia
+    if play_for(perf)['type'] == "comedy":
+        volume_credits += math.floor(perf['audience'] / 5)
+    return volume_credits
 
 
 print(statement())

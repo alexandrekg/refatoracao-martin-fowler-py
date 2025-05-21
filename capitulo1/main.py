0485953f76241418adebbd5395d9c84b7af4f28a
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2025-05-21 11:32:34 -0300

    <refatorando> - mudando o nome do método format_number para usd()

--- capitulo1/main.py
+++ capitulo1/main.py
@@ -12,7 +12,7 @@ def statement():
         # soma créditos por volume
         volume_credits += volume_credits_for(perf)
         # exibe a linha para esta requisição
-        result += f" {play_for(perf)['name']}: {format_number(amount_for(perf) / 100)} ({perf['audience']} seats)\n"
+        result += f" {play_for(perf)['name']}: {usd(amount_for(perf) / 100)} ({perf['audience']} seats)\n"
         total_amount += amount_for(perf)
     result += f"Amount owed is ${'{:,.2f}'.format(total_amount / 100)}\n"
     result += f"You earned {volume_credits} credits\n"
@@ -52,7 +52,7 @@ def volume_credits_for(a_performance):
         result += math.floor(a_performance['audience'] / 5)
     return result
 
-def format_number(a_number):
+def usd(a_number):
     return f"$ "'{:,.2f}'.format(a_number)
 
 

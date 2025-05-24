5f048b86c3e6d8efaabff22d8c081b762ef6e752
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2025-05-24 17:20:53 -0300

    <refactor>

--- capitulo1/main.py
+++ capitulo1/main.py
@@ -5,7 +5,7 @@ import math
 
 
 
-def statement(invoices, plays):
+def statement(invoices):
     statement_data = {}
     statement_data['customer'] = invoices['customer']
     statement_data['performances'] = [enrich_performance(perf) for perf in invoices['performances']]
@@ -14,23 +14,23 @@ def statement(invoices, plays):
 def render_plain_text(statement_data):
     result = f"Statement for {statement_data['customer']} \n"
     for perf in statement_data['performances']:
-        result += f" {play_for(perf)['name']}: {usd(amount_for(perf))} ({perf['audience']} seats)\n"
+        result += f" {perf['play']['name']}: {usd(amount_for(perf))} ({perf['audience']} seats)\n"
 
-    result += f"Amount owed is {usd(total_amount())}\n"
-    result += f"You earned {total_volume_credits()} credits\n"
+    result += f"Amount owed is {usd(total_amount(statement_data))}\n"
+    result += f"You earned {total_volume_credits(statement_data)} credits\n"
     return result
 
 
-def total_amount():
+def total_amount(statement_data):
     result = 0
-    for perf in invoices['performances']:
+    for perf in statement_data['performances']:
         result += amount_for(perf)
     return result
 
 
-def total_volume_credits():
+def total_volume_credits(statement_data):
     result = 0
-    for perf in invoices['performances']:
+    for perf in statement_data['performances']:
         result += volume_credits_for(perf)
     return result
 
@@ -41,9 +41,9 @@ def usd(a_number):
 
 def volume_credits_for(a_performance):
     result = 0
-    result += max(a_performance['audience'] - 30, 0)
+    result += max(a_performance['audience'] - 30, 0)    
     # soma um crédito extra para cada dez espectadores de comédia
-    if play_for(a_performance)['type'] == "comedy":
+    if a_performance['play']['type'] == "comedy":
         result += math.floor(a_performance['audience'] / 5)
     return result
 
@@ -57,25 +57,26 @@ def amount_for(a_performance):
     a_performance - o prefixo 'a_' vem de array, pra mostrar a tipagem da variável
     """
     result = 0
-    if play_for(a_performance)['type'] == "tragedy":
+    if a_performance['play']['type'] == "tragedy":
         result = 40000
         if a_performance['audience'] > 30:
             result += 1000 * (a_performance['audience'] - 30)
-    elif play_for(a_performance)['type'] == "comedy":
+    elif a_performance['play']['type'] == "comedy":
         result = 30000
         if a_performance['audience'] > 20:
             result += 10000 + 500 * (a_performance['audience'] - 20)
         result += 300 * a_performance['audience']
     else:
-        raise Exception(f"unknown type: {play_for(a_performance)['type']}")
+        raise Exception(f"unknown type: {a_performance['play']['type']}")
 
     return result
 
 def enrich_performance(a_performance):
     result = a_performance.copy()
+    result['play'] = play_for(a_performance)
     return result
 
 
 plays = json.load(open('plays.json'))
 invoices = json.load(open('invoices.json'))[0]
-print(statement(invoices, plays))
+print(statement(invoices))

88c0974974b66c26db84d0e8eb9d9af7772715ae
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2025-05-24 22:01:41 -0300

    <refactor> - separando as lógicas em 2 arquivos

---
+++ capitulo1/create_statement_data.py
@@ -0,0 +1,79 @@
+
+import math
+import json
+from functools import reduce
+plays = json.load(open('plays.json'))
+
+
+def render_plain_text(statement_data):
+    result = f"Statement for {statement_data['customer']} \n"
+    for perf in statement_data['performances']:
+        result += f" {perf['play']['name']}: {usd(perf['amount'])} ({perf['audience']} seats)\n"
+
+    result += f"Amount owed is {usd(statement_data['total_amount'])}\n"
+    result += f"You earned {statement_data['total_volume_credits']} credits\n"
+    return result
+
+
+def create_statement_data(invoice):
+    statement_data = {}
+    statement_data['customer'] = invoice['customer']
+    statement_data['performances'] = list(
+        map(enrich_performance, invoice['performances']))
+    statement_data['total_amount'] = total_amount(statement_data)
+    statement_data['total_volume_credits'] = total_volume_credits(
+        statement_data)
+    return statement_data
+
+
+def total_amount(statement_data):
+    return reduce(lambda current_amount, new_amount: current_amount + new_amount['amount'], statement_data['performances'], 0)
+
+
+def total_volume_credits(statement_data):
+    return reduce(lambda volume_credits, new_vol_credits: volume_credits + new_vol_credits['volume_credits'], statement_data['performances'], 0)
+
+
+def usd(a_number):
+    return f"$ "'{:,.2f}'.format(a_number / 100)
+
+
+def volume_credits_for(a_performance):
+    result = 0
+    result += max(a_performance['audience'] - 30, 0)
+    # soma um crédito extra para cada dez espectadores de comédia
+    if a_performance['play']['type'] == "comedy":
+        result += math.floor(a_performance['audience'] / 5)
+    return result
+
+
+def play_for(a_performance):
+    return plays[a_performance['playID']]
+
+
+def amount_for(a_performance):
+    """
+    a_performance - o prefixo 'a_' vem de array, pra mostrar a tipagem da variável
+    """
+    result = 0
+    if a_performance['play']['type'] == "tragedy":
+        result = 40000
+        if a_performance['audience'] > 30:
+            result += 1000 * (a_performance['audience'] - 30)
+    elif a_performance['play']['type'] == "comedy":
+        result = 30000
+        if a_performance['audience'] > 20:
+            result += 10000 + 500 * (a_performance['audience'] - 20)
+        result += 300 * a_performance['audience']
+    else:
+        raise Exception(f"unknown type: {a_performance['play']['type']}")
+
+    return result
+
+
+def enrich_performance(a_performance):
+    result = a_performance.copy()
+    result['play'] = play_for(a_performance)
+    result['amount'] = amount_for(result)
+    result['volume_credits'] = volume_credits_for(result)
+    return result
